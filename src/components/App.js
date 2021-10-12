43442742da2afdee431c301ae3ed055af9ea9536
Deddy Wolley <deddywolley@gmail.com>
2021-10-13 00:29:55 +0800

    update

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,25 @@ import { useState } from 'react'
 import './App.scss'
 import { Chat } from './Chat'
 import { SelectChat } from './SelectChat'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
-	const [selectedChat, setSelectedChat] = useState(null)
 	return (
 		<div className='app dark-theme'>
-			<div className={`app-container ${!selectedChat ? 'app-container__no-chat' : ''}`}>
-				{selectedChat ? <Chat /> : <SelectChat />}
-			</div>
+			<Router>
+				<Switch>
+					<Route path='/' exact>
+						<div className='app-container app-container__compact'>
+							<SelectChat />
+						</div>
+					</Route>
+					<Route path='/:chatId' exact>
+						<div className='app-container'>
+							<Chat />
+						</div>
+					</Route>
+				</Switch>
+			</Router>
 		</div>
 	)
 }
