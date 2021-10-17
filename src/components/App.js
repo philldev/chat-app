07d04f0d6b067146d431d2052b193b63e90846b8
Deddy Wolley <deddywolley@gmail.com>
2021-10-17 15:53:08 +0800

    reset

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,6 @@
-import { useState } from 'react'
-import './App.scss'
-import { Chat } from './Chat'
-import { SelectChat } from './SelectChat'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-
 function App() {
 	return (
-		<div className='app dark-theme'>
-			<Router>
-				<Switch>
-					<Route path='/' exact>
-						<div className='app-container app-container__compact'>
-							<SelectChat />
-						</div>
-					</Route>
-					<Route path='/:chatId' exact>
-						<div className='app-container'>
-							<Chat />
-						</div>
-					</Route>
-				</Switch>
-			</Router>
+		<div>
 		</div>
 	)
 }
--- src/components/MessageInput.js
+++
@@ -1,9 +0,0 @@
-import './MessageInput.scss'
-
-export const MessageInput = (props) => {
-	return (
-		<div className='message-input-wrapper'>
-			<input className='message-input' placeholder='type your message' />
-		</div>
-	)
-}
