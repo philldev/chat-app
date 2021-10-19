ed0474b1c144689b12994d445dd0524ef0ec17ca
Deddy Wolley <deddywolley@gmail.com>
2021-10-19 14:57:41 +0800

    added loader

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,37 @@
-import { Box } from "@chakra-ui/layout"
-import { BrowserRouter, Switch } from "react-router-dom"
-import { Routes } from "../routes"
+import { Box } from '@chakra-ui/layout'
+import { BrowserRouter, Switch } from 'react-router-dom'
+import { Routes } from '../routes'
+import * as React from 'react'
+import { Spinner } from '@chakra-ui/spinner'
 
 function App() {
+	const [isFetchingUser, setIsFetchingUser] = React.useState(true)
+
+	React.useEffect(() => {
+		setTimeout(() => {
+			setIsFetchingUser(false)
+		}, 1000)
+	}, [])
+
 	return (
-		<Box display='grid' placeItems='center' bg='slate.50' h='100vh' w='100vw' color='slate.900'>
-			<BrowserRouter>
-				<Switch>
-					<Routes />
-				</Switch>
-			</BrowserRouter>
+		<Box
+			display='grid'
+			overflow='hidden'
+			placeItems='center'
+			bg='slate.50'
+			h='100vh'
+			w='100vw'
+			color='slate.900'
+		>
+			{isFetchingUser ? (
+				<Spinner size='xl' />
+			) : (
+				<BrowserRouter>
+					<Switch>
+						<Routes />
+					</Switch>
+				</BrowserRouter>
+			)}
 		</Box>
 	)
 }
