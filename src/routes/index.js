8b9713104c67bc228b9a484268a284d297e80947
Deddy Wolley <deddywolley@gmail.com>
2021-10-19 16:06:22 +0800

    added private route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
-import { Route } from 'react-router'
+import { Redirect, Route } from 'react-router'
 import { Chat, HomePage } from '../pages'
 import { auth } from './auth'
 
@@ -7,19 +7,27 @@ const routes = [
 		component: HomePage,
 		path: '/',
 		exact: true,
+		isPrivate: true,
 	},
 	{
-
 		component: Chat,
 		path: '/chat/:chatId',
 		exact: true,
+		isPrivate: true,
 	},
 	...auth,
 ]
 
-export const generateRoutes = () =>
-	routes.map(({ exact, component, path }, key) => (
-		<Route  {...{ exact, component, path, key }} />
-	))
+const PrivateRoute = ({ isLoggedIn, ...props }) =>
+	isLoggedIn ? <Route {...props} /> : <Redirect to='/login' />
 
-export const Routes = () => <>{generateRoutes()}</>
+const generateRoutes = (isLoggedIn) =>
+	routes.map(({ exact, component, path, isPrivate }, key) =>
+		!isPrivate ? (
+			<Route {...{ exact, component, path, key }} />
+		) : (
+			<PrivateRoute {...{ exact, component, path, key, isLoggedIn }} />
+		)
+	)
+
+export const Routes = ({ isLoggedIn }) => <>{generateRoutes(isLoggedIn)}</>
